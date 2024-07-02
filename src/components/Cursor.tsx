473a3aa9f8db8483c7951e3e8d3056cf56cfd778
Nickolas Piraino <kwnp17c@gmail.com>
2024-07-01 21:42:15 -0400

    cursor icon imporved; working on dynamic cursor icon rotation;

--- src/components/Cursor.tsx
+++ src/components/Cursor.tsx
@@ -7,8 +7,8 @@ const Cursor = () => {
         let xTo = gsap.quickTo(".cursor", "x", { duration: 0.4, ease: "power3" }),
             yTo = gsap.quickTo(".cursor", "y", { duration: 0.4, ease: "power3" });
 
-        let shadowX = gsap.quickSetter(".dynamicShadow", "bodShadow"),
-            shadowY = gsap.quickSetter(".dynamicShadow", "bodShadow");
+        let shadowX = gsap.quickSetter(".dynamicShadow", "boxShadow"),
+            shadowY = gsap.getProperty(".dynamicShadow", "boxShadow");
         window.addEventListener("mousemove", e => {
             console.log(e.movementX, e.movementY)
             xTo(e.clientX);
@@ -17,7 +17,9 @@ const Cursor = () => {
     });
     return (
         <>
-            <div className="cursor bg-white w-[5vh] h-[5vh] rounded-full fixed top-0 left-0 -translate-x-[50%] -translate-y-[50%] mix-blend-screen z-10" ><div className="relative w-full h-full"><span className="z-10 bg-color-red w-full h-full fixed" /></div></div>
+            <div className="cursor bg-white w-[5vh] h-[5vh] rounded-full fixed top-0 left-0 -translate-x-[50%] -translate-y-[50%] mix-blend-screen z-10" >
+                <div className=" bg-red-700 absolute w-[20%] h-[20%] top-[15%] left-[45%] rounded-full" />
+            </div>
         </>
     )
 }
